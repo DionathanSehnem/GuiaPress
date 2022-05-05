e0db89afa82daebc7a559774033926e12728a010
Dionathan Sehnem <dionathan2002@gmail.com>
2022-05-05 10:41:28 -0300

    created dynamic page to show articles

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,29 @@ const Article = require('../models/Article.js');
 
 let routes = (app) => {
     app.get('/', (req, res) => {
-        Article.findAll().then((articles) => {
+        Article.findAll({
+            order: [
+                ['id', 'DESC']
+            ]
+        }).then((articles) => {
             res.render('index', { articles: articles });
+        })
+    })
+
+    app.get('/:slug', (req, res) => {
+        const { slug } = req.params;
+        Article.findOne({
+            where: {
+                slug: slug
+            }
+        }).then((article) => {
+            if (article != undefined) {
+                res.render('article', { article: article });
+            } else {
+                res.redirect("/");
+            }
+        }).catch((err) => {
+            res.redirect("/");
         })
     })
 
