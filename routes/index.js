21e12caa4dd1cc436ca224cc792c98bde132ef42
Dionathan Sehnem <dionathan2002@gmail.com>
2022-05-05 11:29:52 -0300

    added function to show articles per category

--- routes/index.js
+++ routes/index.js
@@ -12,8 +12,13 @@ let routes = (app) => {
             order: [
                 ['id', 'DESC']
             ]
-        }).then((articles) => {
-            res.render('index', { articles: articles });
+        }).then(articles => {
+            Category.findAll().then(categories => {
+                res.render('index', {
+                    articles: articles,
+                    categories: categories
+                });
+            })
         })
     })
 
@@ -25,7 +30,36 @@ let routes = (app) => {
             }
         }).then((article) => {
             if (article != undefined) {
-                res.render('article', { article: article });
+                Category.findAll().then(categories => {
+                    res.render('article', {
+                        article: article,
+                        categories: categories
+                    });
+                })
+            } else {
+                res.redirect("/");
+            }
+        }).catch((err) => {
+            res.redirect("/");
+        })
+    })
+
+    app.get('/category/:slug', (req, res) => {
+        const { slug } = req.params;
+        console.log("dsahdkashdlkashdlkjashdlkashdask\n\n\n\n" + slug)
+        Category.findOne({
+            where: {
+                slug: slug
+            },
+            include: [{ model: Article }]
+        }).then((category) => {
+            if (category != undefined) {
+                Category.findAll().then(categories => {
+                    res.render('category', {
+                        articles: category.articles,
+                        categories: categories
+                    });
+                })
             } else {
                 res.redirect("/");
             }
